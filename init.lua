a2cc29e5c236cd04ac6d40a6abf663e50c6e28d5
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2025-04-21 17:34:17 +0000

    feat: more status indicators
    indicators for
    - lev
    - invis
    - silenced
    - feared
    - looting
    
    you can launch with arugments and they will toggle on
    
    example
    `/lua run spritehud 120 female` - will open with size 120 and toggle your sprite to be a female
    
    you can add any of the toggles on the config as an argument at load to have that set to on.

--- init.lua
+++ init.lua
@@ -40,9 +40,9 @@ local lastSpriteFrameTime = mq.gettime()
 local frameWidth = spriteSheetSize / spriteSheetCols  -- total columns (4 male + 4 female)
 local frameHeight = spriteSheetSize / spriteSheetCols -- 8 rows for 8 directions
 local colPerAnimation = spriteSheetCols / 2
+local args = { ..., }
 
-
-local debugBackground = {
+local manualBackground = {
 	indoor = false,
 	outdoor = false,
 	dungeon = false,
@@ -51,7 +51,7 @@ local debugBackground = {
 	underwater = false,
 }
 
-local debugEfx = {
+local manualEfx = {
 	casting = false,
 	cursed = false,
 	poisoned = false,
@@ -65,22 +65,28 @@ local debugEfx = {
 	feetwet = false,
 	underwater = false,
 	feigning = false,
-
+	levitate = false,
+	invis = false,
+	sitting = false,
+	silenced = false,
+	feared = false,
+	looting = false,
 }
 
-local debugCombat = {
+local manualSprite = {
 	skele = false,
 	werewolf = false,
 	wolf = false,
 	bear = false,
 	lich = false,
 	elemental = false,
-	oldCaster = false,
-	noArmor = false,
+	oldcaster = false,
+	noarmor = false,
 	female = false,
 	male = false,
 	combat = false,
 	caster = false,
+	melee = false,
 }
 
 local status = {
@@ -109,6 +115,11 @@ local status = {
 	Bear = false,
 	Wolf = false,
 	Lich = false,
+	Levitate = false,
+	Invis = false,
+	Silenced = false,
+	Feared = false,
+	Looting = false,
 }
 
 -- directions to row mapping for the sprite sheet
@@ -122,6 +133,24 @@ local directions = {
 	[6] = 1, -- West
 	[7] = 6, -- NW
 }
+
+function GetInvisCell(directionVal)
+	if directionVal == 3 or directionVal == 0 then
+		return 0
+	elseif directionVal == 1 then
+		return 2
+	elseif directionVal == 2 then
+		return 1
+	elseif directionVal == 4 then
+		return 4
+	elseif directionVal == 5 then
+		return 3
+	elseif directionVal == 6 then
+		return 5
+	elseif directionVal == 7 then
+		return 6
+	end
+end
 
 local casters = {
 	['WIZ'] = true,
@@ -139,11 +168,36 @@ local function LoadImages()
 	efx2Texture = mq.CreateTexture(filePath .. 'efx_overlay_sheet2_1k.png')
 end
 
+local function checkArgs()
+	if #args == 0 then
+		return
+	end
+
+	for _, arg in ipairs(args) do
+		if tonumber(arg) then
+			imgSize = tonumber(arg)
+			if imgSize < 1 then
+				imgSize = 1
+			end
+		elseif manualSprite[arg] ~= nil then
+			manualSprite[arg] = true
+			printf("\aw[\at%s\ax] \aySetting \ax(\at%s\ax) to enabled.\ax", Module.Name, arg)
+		elseif manualEfx[arg] ~= nil then
+			manualEfx[arg] = true
+			printf("\aw[\at%s\ax] \aySetting \ax(\at%s\ax) to enabled.\ax", Module.Name, arg)
+		end
+		if manualBackground[arg] ~= nil then
+			manualBackground[arg] = true
+			printf("\aw[\at%s\ax] \aySetting Background \ax(\at%s\ax) to enabled.\ax", Module.Name, arg)
+		end
+	end
+end
+
 local function Init()
 	Module.IsRunning = true
 
 	mq.bind("/spritehud", Module.CommandHandler)
-
+	checkArgs()
 	Module.UpdateStatus()
 	mq.imgui.init(Module.Name, Module.RenderGUI)
 
@@ -204,7 +258,7 @@ function Module.RenderConfig()
 		imgSize = ImGui.DragInt("Sprite Size##SpriteSize", imgSize, 1, 1, 512)
 		targetFPS = ImGui.DragInt("Animation Speed##SpriteFPS", targetFPS, 1, 1, 30)
 		local cnt = 1
-		ImGui.SeparatorText("Debug EFX:")
+		ImGui.SeparatorText("EFX:")
 		if ImGui.BeginTable("##DebugEfx", 4) then
 			ImGui.TableNextRow()
 			ImGui.TableNextColumn()
@@ -212,13 +266,13 @@ function Module.RenderConfig()
 				ImGui.PushID(data.name)
 				ImGui.Text(data.name)
 				ImGui.TableNextColumn()
-				debugEfx[data.name] = ImGui.Checkbox("##Dbg", data.value)
+				manualEfx[data.name] = ImGui.Checkbox("##Dbg", data.value)
 				ImGui.TableNextColumn()
 				ImGui.PopID()
 			end
 			ImGui.EndTable()
 		end
-		ImGui.SeparatorText("Debug Background:")
+		ImGui.SeparatorText("Background:")
 		if ImGui.BeginTable("##DebugBackground", 4) then
 			ImGui.TableNextRow()
 			ImGui.TableNextColumn()
@@ -226,13 +280,13 @@ function Module.RenderConfig()
 				ImGui.PushID(data.name)
 				ImGui.Text(data.name)
 				ImGui.TableNextColumn()
-				debugBackground[data.name] = ImGui.Checkbox("##Dbg", data.value)
+				manualBackground[data.name] = ImGui.Checkbox("##Dbg", data.value)
 				ImGui.TableNextColumn()
 				ImGui.PopID()
 			end
 			ImGui.EndTable()
 		end
-		ImGui.SeparatorText("Debug Combat:")
+		ImGui.SeparatorText("Sprite:")
 		if ImGui.BeginTable("##DebugCombat", 4) then
 			ImGui.TableNextRow()
 			ImGui.TableNextColumn()
@@ -240,7 +294,7 @@ function Module.RenderConfig()
 				ImGui.PushID(data.name)
 				ImGui.Text(data.name)
 				ImGui.TableNextColumn()
-				debugCombat[data.name] = ImGui.Checkbox("##Dbg", data.value)
+				manualSprite[data.name] = ImGui.Checkbox("##Dbg", data.value)
 				ImGui.TableNextColumn()
 				ImGui.PopID()
 			end
@@ -280,12 +334,18 @@ function Module.RenderGUI()
 			-------- Sprite Animation Style
 			local heading = myself.Heading.Degrees() or 0
 			local rowNum = HeadingToRowNum(heading)
+			local drawFrame = currentFrame
+
+			if not status.Moving then
+				drawFrame = 0
+			end
+
 			cursorX, cursorY = ImGui.GetCursorPos()
 
 			-- draw background
 			if status.UnderWater then
 				DrawAnimatedFrame(efxTexture, 3, 3, false)
-			elseif status.Indoor or status.Outside and not debugBackground.dungeon then
+			elseif status.Indoor or status.Outside and not manualBackground.dungeon then
 				if status.Night then
 					DrawAnimatedFrame(efxTexture, 3, 2, true)
 				else
@@ -302,7 +362,20 @@ function Module.RenderGUI()
 
 
 			-- Draw the sprite
-			if status.Sitting then
+			if status.Invis then
+				local invisCol = GetInvisCell(rowNum)
+				local invisOffset = invisCol > 3
+				if invisCol > 3 then
+					invisCol = invisCol - rightOffset
+				end
+				if not status.Sitting then
+					DrawAnimatedFrame(efxTexture, 6, invisCol, invisOffset)
+				else
+					DrawAnimatedFrame(efxTexture, 6, 3, true)
+				end
+			elseif status.Looting then
+				DrawAnimatedFrame(efxTexture, 7, 3, false)
+			elseif status.Sitting then
 				local colIdx = isFemale and 3 or 2
 				if status.Caster then
 					DrawAnimatedFrame(efx2Texture, 3, colIdx, false)
@@ -317,11 +390,6 @@ function Module.RenderGUI()
 					DrawAnimatedFrame(efx2Texture, 3, colIdx, true)
 				end
 			else
-				local drawFrame = currentFrame
-				if not status.Moving then
-					drawFrame = 0
-				end
-
 				if status.Combat then
 					if status.Werewolf then
 						DrawAnimatedFrame(efx2Texture, 0, efxFrame, false)
@@ -336,20 +404,20 @@ function Module.RenderGUI()
 					elseif status.Elemental then
 						DrawAnimatedFrame(efx2Texture, 2, efxFrame, false)
 					elseif status.Caster then
-						if debugCombat.oldCaster then
+						if manualSprite.oldcaster then
 							DrawAnimatedFrame(efx2Texture, 6, currentFrame, isFemale)
 						else
 							DrawAnimatedFrame(efx2Texture, 5, currentFrame, isFemale)
 						end
 					else
-						if debugCombat.noArmor then
+						if manualSprite.noarmor then
 							DrawAnimatedFrame(efx2Texture, 7, currentFrame, isFemale)
 						else
 							DrawAnimatedFrame(efx2Texture, 4, currentFrame, isFemale)
 						end
 					end
 				elseif status.ResSick or status.Hovering then
-					DrawAnimatedFrame(efxTexture, 0, efxFrame, true)
+					DrawAnimatedFrame(efxTexture, 0, 0, true)
 				elseif status.Caster then
 					DrawAnimatedFrame(casterTexture, rowNum, drawFrame, isFemale)
 				else
@@ -375,7 +443,7 @@ function Module.RenderGUI()
 			end
 
 			if status.Casting then
-				if debugEfx.casting then
+				if manualEfx.casting then
 					DrawAnimatedFrame(efxTexture, 5, efxFrame, true)
 				elseif mq.TLO.Spell(myself.Casting()).Category() == 'Create Item' or mq.TLO.Spell(myself.Casting()).Category() == 'Pet' then
 					DrawAnimatedFrame(efxTexture, 7, efxFrame, true)
@@ -392,10 +460,22 @@ function Module.RenderGUI()
 				DrawAnimatedFrame(efxTexture, 4, efxFrame, false)
 			end
 
+			if status.Silenced then
+				DrawAnimatedFrame(efxTexture, 0, 2, true)
+			end
+
+			if status.Feared then
+				DrawAnimatedFrame(efxTexture, 0, 1, true)
+			end
+
 			if status.FeetWet and not status.UnderWater then
 				DrawAnimatedFrame(efxTexture, 3, 1, false)
-			elseif (status.FeetWet and status.UnderWater) or debugEfx.underwater then
+			elseif (status.FeetWet and status.UnderWater) or manualEfx.underwater then
 				DrawAnimatedFrame(efxTexture, 3, 2, false)
+			end
+
+			if status.Levitate then
+				DrawAnimatedFrame(efxTexture, 3, 0, false)
 			end
 
 			-- draw the border
@@ -429,11 +509,11 @@ function Module.CommandHandler(...)
 	elseif args[1] == 'config' then
 		Module.ShowConfig = not Module.ShowConfig
 	elseif args[1] == 'debug' then
-		if debugEfx[args[2]] ~= nil then
-			debugEfx[args[2]] = not debugEfx[args[2]]
+		if manualEfx[args[2]] ~= nil then
+			manualEfx[args[2]] = not manualEfx[args[2]]
 		end
 		local found = false
-		for k, v in pairs(debugEfx) do
+		for k, v in pairs(manualEfx) do
 			if v then
 				found = true
 				break
@@ -481,42 +561,48 @@ function Module.UpdateStatus()
 	if not Module.ShowGui then return end
 	local myClass = myself.Class.ShortName() or "Unknown"
 	local zoneType = mq.TLO.Zone.Type() or 0
-	status.Combat = debugCombat.combat or myself.Combat()
-	status.Sitting = myself.Sitting() or false
-	status.FeetWet = debugEfx.feetwet or myself.FeetWet()
-	status.UnderWater = debugBackground.underwater or myself.Underwater()
-	status.Dungeon = debugBackground.dungeon or mq.TLO.Zone.Dungeon()
-	status.Outside = debugBackground.outside or mq.TLO.Zone.Outdoor()
-	status.Casting = debugEfx.casting or myself.Casting()
-	status.Night = debugBackground.night or mq.TLO.GameTime.Night()
-	status.Poisoned = debugEfx.poisoned or myself.Poisoned()
-	status.Diseased = debugEfx.diseased or myself.Diseased()
-	status.Cursed = debugEfx.cursed or myself.Cursed()
-	status.Mezzed = debugEfx.mezzed or myself.Mezzed()
-	status.Stunned = debugEfx.stunned or myself.Stunned()
-	status.Snared = debugEfx.snared or myself.Snared()
-	status.Rooted = debugEfx.rooted or myself.Rooted()
-	status.Hovering = debugEfx.hovering or myself.Hovering()
-	status.ResSick = debugEfx.ressick or myself.Buff("Resurrection Sickness")() ~= nil
-	status.Indoor = debugBackground.indoor or (zoneType == 3 or zoneType == 4)
-	status.Feigning = debugEfx.feigning or myself.Feigning()
-	status.Skele = debugCombat.skele or myself.Race() == 'Skeleton'
-	status.Werewolf = debugCombat.werewolf or myself.Race() == 'Werewolf'
-	status.Wolf = debugCombat.wolf or myself.Race() == 'Wolf'
-	status.Bear = debugCombat.bear or myself.Race() == 'Bear'
-	status.Lich = debugCombat.lich or myself.Race() == 'Lich'
-	status.Elemental = debugCombat.elemental or myself.Race() == 'Elemental'
+	status.Combat = manualSprite.combat or myself.Combat()
+	status.Sitting = manualEfx.sitting or myself.Sitting()
+	status.FeetWet = manualEfx.feetwet or myself.FeetWet()
+	status.UnderWater = manualBackground.underwater or myself.Underwater()
+	status.Dungeon = manualBackground.dungeon or mq.TLO.Zone.Dungeon()
+	status.Outside = manualBackground.outside or mq.TLO.Zone.Outdoor()
+	status.Casting = manualEfx.casting or myself.Casting()
+	status.Night = manualBackground.night or mq.TLO.GameTime.Night()
+	status.Poisoned = manualEfx.poisoned or myself.Poisoned()
+	status.Diseased = manualEfx.diseased or myself.Diseased()
+	status.Cursed = manualEfx.cursed or myself.Cursed()
+	status.Mezzed = manualEfx.mezzed or myself.Mezzed()
+	status.Stunned = manualEfx.stunned or myself.Stunned()
+	status.Snared = manualEfx.snared or myself.Snared()
+	status.Rooted = manualEfx.rooted or myself.Rooted()
+	status.Hovering = manualEfx.hovering or myself.Hovering()
+	status.ResSick = manualEfx.ressick or myself.Buff("Resurrection Sickness")() ~= nil
+	status.Indoor = manualBackground.indoor or (zoneType == 3 or zoneType == 4)
+	status.Feigning = manualEfx.feigning or myself.Feigning()
+	status.Skele = manualSprite.skele or myself.Race() == 'Skeleton'
+	status.Werewolf = manualSprite.werewolf or myself.Race() == 'Werewolf'
+	status.Wolf = manualSprite.wolf or myself.Race() == 'Wolf'
+	status.Bear = manualSprite.bear or myself.Race() == 'Bear'
+	status.Lich = manualSprite.lich or myself.Race() == 'Spectre'
+	status.Elemental = manualSprite.elemental or myself.Race() == 'Elemental'
+	status.Levitate = manualEfx.levitate or myself.Levitating()
+	status.Invis = manualEfx.invis or myself.Invis()
+	status.Silenced = manualEfx.silenced or myself.Silenced()
+	status.Feared = manualEfx.feared or (myself.Feared() or false)
+	status.Looting = manualEfx.looting or mq.TLO.Corpse() ~= 'FALSE'
 
-
-	if casters[myClass] ~= nil or debugCombat.caster then
+	if casters[myClass] ~= nil or manualSprite.caster then
 		status.Caster = true
 	else
 		status.Caster = false
 	end
+	if manualSprite.melee then
+		status.Caster = false
+	end
 
 
-
-	if debugBackground.day then
+	if manualBackground.day then
 		status.Night = false
 	end
 
@@ -530,8 +616,8 @@ function Module.UpdateStatus()
 	local buffCount = myself.BuffCount() or 0
 	local songCount = myself.CountSongs() or 0
 
-	isFemale = debugCombat.female or myself.Gender() == 'female'
-	if debugCombat.male then
+	isFemale = manualSprite.female or myself.Gender() == 'female'
+	if manualSprite.male then
 		isFemale = false
 	end
 
@@ -558,19 +644,19 @@ function Module.UpdateStatus()
 			end
 		end
 	end
-	hasHoT = checkHot or debugEfx.hot
+	hasHoT = checkHot or manualEfx.hot
 	sortedTbl = {}
-	for k, v in pairs(debugEfx) do
+	for k, v in pairs(manualEfx) do
 		table.insert(sortedTbl, { name = k, value = v, })
 	end
 	table.sort(sortedTbl, function(a, b) return a.name < b.name end)
 	sortedBackground = {}
-	for k, v in pairs(debugBackground) do
+	for k, v in pairs(manualBackground) do
 		table.insert(sortedBackground, { name = k, value = v, })
 	end
 	table.sort(sortedBackground, function(a, b) return a.name < b.name end)
 	sortedCombat = {}
-	for k, v in pairs(debugCombat) do
+	for k, v in pairs(manualSprite) do
 		table.insert(sortedCombat, { name = k, value = v, })
 	end
 	table.sort(sortedCombat, function(a, b) return a.name < b.name end)
